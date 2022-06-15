247f2e154db1e13f8781a6f923111869d887a397
Tay811 <mataitaya@gmail.com>
2022-06-15 17:22:25 +0300

    first_2 commit

--- script.js
+++ script.js
@@ -5,30 +5,31 @@ button_01.onclick = function reckoning() {
     firstNum = +prompt('Введите первое число');
     secondNum = +prompt('Введите второе число');
      if ( isNaN(firstNum) || isNaN(secondNum)) {
-           return alert('Некорректный ввод');
+        return console.log('Некорректный ввод');
      }
      if (secondNum === 2){
-         let result = firstNum.toString(2);
-        return alert (result);
+        let result = firstNum.toString(2);
+        return console.log (result);
+         
      }
      if (secondNum === 8){
         let result = firstNum.toString(8);
-        return alert (result);
+        return console.log (result);
     }
     if (secondNum === 10){
         let result = firstNum.toString(10);
-        return alert (result);
+        return console.log (result);
     }
      if (secondNum === 16){
         let result = firstNum.toString(16);
-        return alert (result);
+        return console.log (result);
     }
     if (secondNum === 32){
-        let result = firstNum.toString(32);
-        return alert (result);
+        let result = firstNum.toString(32);    
+        return console.log (result); 
     }
     else {
-        return alert('Некорректный ввод');
+        return console.log('Некорректный ввод');
     }
     }
     
@@ -41,16 +42,16 @@ button_02.onclick = function result() {
         
     firstNum = +prompt('Введите первое число');
     if ( isNaN(firstNum)) {
-        return alert('Некорректный ввод');
+        return console.log('Некорректный ввод');
         }
     else (secondNum = +prompt('Введите второе число'));
     if ( isNaN(secondNum)) {
-        return alert('Некорректный ввод');    
+        return console.log('Некорректный ввод');    
          }
     else {
         sum = firstNum + secondNum;
         quo = firstNum / secondNum;
-    alert([sum + ', ' + quo]);
+        return console.log([sum + ', ' + quo]);
     };
     
     }
